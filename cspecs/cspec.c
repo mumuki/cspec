b1a6c05a9673b2f5c69503b34bbb80ed878c1897
Federico Scarpa <fedescarpa@gmail.com>
2014-07-20 18:18:11 -0300

    Adding Fancy Report

--- cspecs/cspec.c
+++ cspecs/cspec.c
@@ -11,19 +11,37 @@ __attribute__((constructor)) void init() {
 
 #define MAX_SHOULDS_PER_IT 64
 
+#define DESCRIBE_COLOR      "\x1b[0m"
+#define FAILURE_COLOR       "\x1b[38;5;1m"
+#define SUCCESS_COLOR       "\x1b[38;5;2m"
+#define PENDING_COLOR       "\x1b[38;5;3m"
+#define NO_COLOR            "\x1b[0m"
+#define UNDERLINE           "\x1b[4m"
+
+#define FAILURE_BULLET      "✖ "
+#define SUCCESS_BULLET      "✔ "
+#define PENDING_BULLET      "• "
+#define DESCRIBE_BULLET     ""
+
+
 typedef struct _should {
   Int     line;
   String  file;
-  char*  error;
+  String  error;
 } Should;
 
 typedef struct _it {
-    char* description;
+    String _describe;
+    String description;
+    Bool is_failure;
     Should* shoulds[MAX_SHOULDS_PER_IT];
 } It;
 
-Int   IT_COUNT      = 0;
-Int   SHOULD_COUNT  = 0;
+Int IT_COUNT            = 0;
+Int SHOULD_COUNT        = 0;
+Int DESCRIBE_DEEP_LEVEL = 0;
+
+String CURRENT_DESCRIBE = "";
 
 It**  ITS;
 
@@ -32,9 +50,20 @@ It**  ITS;
 // ---------------------------------------------------------------------------
 
     char*   __get_template (Bool neg, String format);
-    It*     __it_create   (String description);
-    int     __report      (void);
-    Should* __should_create(char* error, String file, Int line);
+    It*     __it_create    (String description);
+    It*     __current_it   ();
+    int     __report       (void);
+    Should* __should_create(String error, String file, Int line);
+    void    __print_current_it_result();
+    void    __print_it_result_detail(It* _it);
+    char*   __get_spaces();
+
+    #define print_description(description, add_desc, spaces_str, separator, type) { \
+        char* spaces = __get_spaces();                                              \
+        printf("%s%s%s%s%s%s%s%s\n", spaces, spaces_str, type##_##COLOR,            \
+                type##_##BULLET, description, separator, add_desc, NO_COLOR);       \
+        free(spaces);                                                               \
+    }
 
 // ---------------------------------------------------------------------------
 // ----- STUB MAIN -----
@@ -49,7 +78,10 @@ It**  ITS;
 // ---------------------------------------------------------------------------
 
     void __describe_pre(String description) {
-        puts(description);
+        if (DESCRIBE_DEEP_LEVEL == 0) puts("");
+        DESCRIBE_DEEP_LEVEL++;
+        CURRENT_DESCRIBE = description;
+        print_description(description, "", "", "", DESCRIBE);
     }
 
     void __describe(String description, Function function) {
@@ -59,7 +91,7 @@ It**  ITS;
     }
 
     void __describe_post(String description) {
-
+        DESCRIBE_DEEP_LEVEL--;
     }
 
 // ---------------------------------------------------------------------------
@@ -67,9 +99,9 @@ It**  ITS;
 // ---------------------------------------------------------------------------
 
     void __it_pre(String description) {
-        SHOULD_COUNT = 0;
         ITS = realloc(ITS, sizeof(It*) * (++IT_COUNT));
         ITS[IT_COUNT - 1] = __it_create(description);
+        SHOULD_COUNT = 0;
     }
 
     void __it(String description, Function function) {
@@ -79,25 +111,36 @@ It**  ITS;
     }
 
     void __it_post(String description) {
-        puts(description);
+        __print_current_it_result();
     }
 
 // ---------------------------------------------------------------------------
 // ----- SHOULDS -----
 // ---------------------------------------------------------------------------
 
-    void __should(String format, String file, Int line, Int actual, Bool negated, Int expected) {
-      Bool comparator = negated ? actual == expected : actual != expected;
-      if (comparator) {
-          char* template = __get_template(negated, format);
-          int message_size = fprintf(devNull, template, expected, actual);
-          char* error = malloc(message_size + 1);
-          sprintf(error, template, expected, actual);
-          ITS[IT_COUNT - 1]->shoulds[SHOULD_COUNT++] = __should_create(error, file, line);
-          free(template);
-          free(error);
-      }
-    }
+    #define __should_definition(suffix, type, comparator, format)                                   \
+        void __should_##suffix(String file, Int line, type actual, Bool negated, type expected) {   \
+            It* _it = __current_it();                                                               \
+            _it->is_failure = negated ? (comparator) : !(comparator);                               \
+            if (_it->is_failure) {                                                                  \
+                String template = __get_template(negated, format);                                  \
+                int message_size = fprintf(devNull, template, expected, actual);                    \
+                String error = malloc(message_size + 1);                                            \
+                sprintf(error, template, expected, actual);                                         \
+                _it->shoulds[SHOULD_COUNT++] = __should_create(error, file, line);                  \
+                free(template);                                                                     \
+            }                                                                                       \
+        }                                                                                           \
+
+    __should_definition(bool  , String, strcmp(actual, expected) == 0 , "%s");
+    __should_definition(char  , char  , actual == expected            , "%c");
+    __should_definition(short , short , actual == expected            , "%i");
+    __should_definition(int   , int   , actual == expected            , "%i");
+    __should_definition(long  , long  , actual == expected            , "%l");
+    __should_definition(double, double, actual == expected            , "%f");
+    __should_definition(float , float , actual == expected            , "%f");
+    __should_definition(ptr   , void* , actual == expected            , "%p");
+    __should_definition(string, String, strcmp(actual, expected) == 0 , "%s");
 
 // ---------------------------------------------------------------------------
 // ----- PRIVATE -----
@@ -105,42 +148,93 @@ It**  ITS;
 
     It* __it_create(String description) {
         It* self = malloc(sizeof(It));
-        self->description = strdup(description);
+        self->description = description;
+        self->_describe = CURRENT_DESCRIBE;
+        self->is_failure = false;
         int i; for (i = 0; i < MAX_SHOULDS_PER_IT; self->shoulds[i++] = NULL);
         return self;
     }
 
-    Should* __should_create(char* error, String file, Int line) {
+    void __it_destroy(It* self) {
+        free(self);
+    }
+
+    Should* __should_create(String error, String file, Int line) {
         Should* self = malloc(sizeof(Should));
-        self->error = strdup(error);
+        self->error = error;
         self->file = file;
         self->line = line;
         return self;
     }
 
+    void __should_destroy(Should* self) {
+        free(self->error);
+        free(self);
+    }
+
+    #define VALUE UNDERLINE "%s" NO_COLOR
+
     char* __get_template(Bool neg, String format) {
-        char* template = malloc(64);
-        memset(template, '\0', 64);
-        sprintf(template, "%sxpected <%s> but was <%s>", !neg ? "E" : "Not e", format, format);
+        char* template = malloc(128);
+        memset(template, '\0', 128);
+        sprintf(template, "%sxpected <" VALUE "> but was <" VALUE ">", !neg ? "E" : "Not e", format, format);
         return template;
     }
 
     int __report(void) {
-        puts("");
-        int i; for (i = 0; i < IT_COUNT; i++) {
+        puts("\n");
+        puts("  " "\x1b[4m" "Summary\n" NO_COLOR);
+        int i, failure_count = 0;
+        for (i = 0; i < IT_COUNT; i++) {
             It* _it = ITS[i];
-            puts(_it->description);
-            int j; for (j = 0; j < MAX_SHOULDS_PER_IT && _it->shoulds[j] != NULL; j++) {
-                Should* _should = _it->shoulds[j];
-                printf("%s - <%s:%d>\n", _should->error, _should->file, _should->line);
-                free(_should->error);
-                free(_should);
+            if (_it->is_failure) {
+                failure_count++;
+                __print_it_result_detail(_it);
             }
-            free(_it->description);
-            free(_it);
+            __it_destroy(_it);
         }
         free(ITS);
         fclose(devNull);
-        return 0;
+        printf(SUCCESS_COLOR "  %d success\n" NO_COLOR, IT_COUNT - failure_count);
+        if (failure_count > 0) printf(FAILURE_COLOR "  %d failure\n" NO_COLOR, failure_count);
+        puts("");
+        return failure_count;
+    }
+
+    It* __current_it() {
+        return ITS[IT_COUNT - 1];
+    }
+
+// ---------------------------------------------------------------------------
+// ----- FANCY REPORT -----
+// ---------------------------------------------------------------------------
+
+    void __print_current_it_result() {
+        It* _it = __current_it();
+        if (_it->is_failure) {
+            print_description(_it->description, "", "  ", "", FAILURE);
+        } else {
+            print_description(_it->description, "", "  ", "", SUCCESS);
+        }
+    }
+
+    void __print_it_result_detail(It* _it) {
+      print_description(_it->_describe, _it->description, "    ", " - ", FAILURE);
+      int j; for (j = 0; j < MAX_SHOULDS_PER_IT && _it->shoulds[j] != NULL; j++) {
+          Should* _should = _it->shoulds[j];
+          char* spaces = __get_spaces();
+          printf("%s      - %s [%s:%d]\n", spaces, _should->error, _should->file, _should->line);
+          __should_destroy(_should);
+          free(spaces);
+      }
+      puts("");
+    }
+
+    char* __get_spaces() {
+        int spaces_count = 2 * DESCRIBE_DEEP_LEVEL;
+        char* spaces = malloc(spaces_count + 1);
+        memset(spaces, ' ', spaces_count);
+        spaces[spaces_count] = '\0';
+        return spaces;
     }
 
--- cspecs/cspec.h
+++ cspecs/cspec.h
@@ -13,7 +13,7 @@
     // ----- TYPES -----
     // ---------------------------------------------------------------------------
 
-        typedef const char* String;
+        typedef char* String;
         typedef void(*Function)(void);
         typedef bool Bool;
         typedef int Int;
@@ -30,7 +30,18 @@
         void __it     (String description, Function function);
         void __it_post(String description);
 
-        void __should(String format, String file, Int line, Int actual, Bool negated, Int expected);
+        #define __should_declaration(suffix, type)                                                  \
+            void __should_##suffix(String file, Int line, type actual, Bool negated, type expected)
+
+        __should_declaration(bool  , String);
+        __should_declaration(char  , char  );
+        __should_declaration(short , short );
+        __should_declaration(int   , int   );
+        __should_declaration(long  , long  );
+        __should_declaration(double, double);
+        __should_declaration(float , float );
+        __should_declaration(ptr   , void* );
+        __should_declaration(string, char* );
 
     // ---------------------------------------------------------------------------
     // ----- MACROS API -----
@@ -41,18 +52,34 @@
         #define describe    __describe(
         #define it          __it      (
 
-        #define begin       , ({ void __$__() {
+        #define do          , ({ void __$__() {
         #define end         }; __$__;}));
 
     // ---------------------------------------------------------------------------
     // ----- MACROS SHOULD -----
     // ---------------------------------------------------------------------------
 
-        #define should(actual)    __should("%i", __FILE__, __LINE__, actual,
-        #define be                false
+        #define __should_call(suffix, actual)   __should_##suffix(__FILE__, __LINE__, (actual),
+
+        #define should_bool(actual)             __should_call(bool  , (actual) ? "true" : "false")
+        #define should_char(actual)             __should_call(char  , actual)
+        #define should_short(actual)            __should_call(short , actual)
+        #define should_int(actual)              __should_call(int   , actual)
+        #define should_long(actual)             __should_call(long  , actual)
+        #define should_double(actual)           __should_call(double, actual)
+        #define should_float(actual)            __should_call(float , actual)
+        #define should_ptr(actual)              __should_call(ptr   , actual)
+        #define should_string(actual)           __should_call(string, actual)
+
+        #define should(actual)    should_int(actual)
+
         #define not               !
+        #define be                false
+
+        #define truthy            , "true")
+        #define falsey            , "false")
+        #define null              , NULL)
         #define equal             ,
         #define to(expected)      expected)
-
 
 #endif /* CSPEC_H_ */
